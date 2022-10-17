f7c6c1c8ec29b30f448981915b2d395d6c096453
Jaime Nufio <jaimeenufio@gmail.com>
2022-10-16 23:52:29 -0400

    boilerplate fixes

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
@@ -14,7 +12,7 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-    client.commands.set(command.data.name, command);
+	client.commands.set(command.data.name, command);
 }
 
 client.once('ready', () => {
