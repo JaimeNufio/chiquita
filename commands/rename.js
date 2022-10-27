868485b2da5aa0ab4fcb66da6f603a8e51b808fe
Jaime Nufio <jaimeenufio@gmail.com>
2022-10-26 22:17:13 -0400

    fixed bug where old name was undefined

--- commands/rename.js
+++ commands/rename.js
@@ -10,9 +10,10 @@ module.exports = {
 	
         async execute(interaction) {
 
+            try{
             const target = interaction.options.getUser('target');
             const nickname = interaction.options.getString('nickname')
-            const oldNickname = interaction.guild.members.fetch(target.id).nickname
+            const oldNickname = (await interaction.guild.members.fetch(target.id)).nickname
 
             if (nickname.length > 32 ){
                 interaction.reply( {"embeds": [{
@@ -56,81 +57,42 @@ module.exports = {
                 return
             }
 
-            // try{
-                userInGuild = (await interaction.guild.members.fetch(target.id)).setNickname(nickname)
-                // console.log(userInGuild)
-                interaction.reply( {"embeds": [{
-                    // "title": "Error: Nickname too long.",
-                    // "description": "Can't update username, exceeds character limit of 32.",
-                    "color": 39129,
-                    "author": {
-                        "name": `${interaction.member.nickname} renamed ${target.username}`,
-                        "icon_url": `${interaction.user.displayAvatarURL({ dynamic: true })}`
-                    },
-                    "thumbnail": {
-                        "url": `${target.displayAvatarURL({ dynamic: true })}`
-                    },
-                    // "footer": {
-                    //     "text": "If you think this is incorrect, bug Jaime.",
-                    //     "icon_url": ""
-                    // },
-                    "fields": [
-                        {
-                            "name": "New Name",
-                            "value": `${nickname}`,
-                            "inline": true
-                        },
-                        {
-                            "name": "Old Name",
-                            "value": `${oldNickname}`,
-                            "inline": true
-                        }
-                    ]
-                }]})
-            // }catch(err){
+            
 
-            // }
-            // if (user) return interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
-            // return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL()}`);
-        
-            // interaction.reply(
-            //     {
-            //         "embeds": [
-            //             {
-            //                 "color": 39129,
-            //                 "timestamp": "2022-10-17T01:51:35.965Z",
-            //                 "url": "https://discord.com",
-            //                 "author": {
-            //                     "name": "Author name",
-            //                     "url": "https://discord.com",
-            //                     "icon_url": "https://i.ibb.co/BnfFDYk/2b5dda36047d120000d320aae1c511acd7bbeae9.png"
-            //                 },
-            //                 "thumbnail": {
-            //                     "url": "https://cdn.discordapp.com/embed/avatars/0.png"
-            //                 },
-            //                 "image": {
-            //                     "url": ""
-            //                 },
-            //                 "footer": {
-            //                     "text": "Footer text",
-            //                     "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
-            //                 },
-            //                 "fields": [
-            //                     {
-            //                         "name": "Field 1, *lorem* **ipsum**, ~~dolor~~",
-            //                         "value": "Field value",
-            //                         "inline": true
-            //                     },
-            //                     {
-            //                         "name": "Another field",
-            //                         "value": "Nope, didn't forget about this",
-            //                         "inline": true
-            //                     }
-            //                 ]
-            //             }
-            //         ]
-            //     }
+            userInGuild = (await interaction.guild.members.fetch(target.id)).setNickname(nickname)
+            // console.log(userInGuild)
+            interaction.reply( {"embeds": [{
+                // "title": "Error: Nickname too long.",
+                // "description": "Can't update username, exceeds character limit of 32.",
+                "color": 39129,
+                "author": {
+                    "name": `${interaction.member.nickname} renamed ${target.username}`,
+                    "icon_url": `${interaction.user.displayAvatarURL({ dynamic: true })}`
+                },
+                "thumbnail": {
+                    "url": `${target.displayAvatarURL({ dynamic: true })}`
+                },
+                // "footer": {
+                //     "text": "If you think this is incorrect, bug Jaime.",
+                //     "icon_url": ""
+                // },
+                "fields": [
+                    {
+                        "name": "New Name",
+                        "value": `${nickname}`,
+                        "inline": true
+                    },
+                    {
+                        "name": "Old Name",
+                        "value": `${oldNickname}`,
+                        "inline": true
+                    }
+                ]
+            }]})
 
-            // )
+            }catch (err){
+                // interaction.reply(err)
+                console.log(err)
+            }
         },
 };
